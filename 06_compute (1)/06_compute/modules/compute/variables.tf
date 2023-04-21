6700be00f281bb6a901d55ff950e4a78aaba7c2b
emma Begard <emma1.begard@gmail.com>
2023-04-21 13:44:55 +0200

    depuis pc ecole

---
+++ 06_compute (1)/06_compute/modules/compute/variables.tf
@@ -0,0 +1,15 @@
+variable "project" {
+  type = string
+}
+
+variable "vpc_id" {
+  type = string
+}
+
+variable "public_subnet_id" {
+  type = string
+}
+
+variable "private_subnet_id" {
+  type = string
+}
